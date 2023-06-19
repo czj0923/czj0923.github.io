73c1f7c8d377bf090c7f6faa2a46525c865ba18e
Cao Zhijie <1051029537@qq.com>
2023-06-19 10:31:00 +0800

    update

--- .vuepress/components/Map.vue
+++ .vuepress/components/Map.vue
@@ -1,32 +1,49 @@
 <template>
-    <div>
-        <div id="container"></div> 
-    </div>
+  <div>
+    <div id="container"></div>
+  </div>
 </template>
 <script>
 export default {
-    components: {
+  components: {
 
-    },
-    mounted() {
-        var map = new BMapGL.Map("container");          // 创建地图实例 
-        var point = new BMapGL.Point(116.404, 39.915);  // 创建点坐标 
-        map.centerAndZoom(point, 15);                 // 初始化地图，设置中心点坐标和地图级别
-    },
-    data() {
-        return {
+  },
+  mounted() {
+    var map = new BMapGL.Map("container");          // 创建地图实例 
+    var point = new BMapGL.Point(116.404, 39.915);  // 创建点坐标 
+    map.centerAndZoom(point, 15);                 // 初始化地图，设置中心点坐标和地图级别
+    map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
 
-        }
-    },
-    methods: {
+    map.addEventListener('click', (e) => {
+      console.log('点击的经纬度：' + e.latlng.lng + ', ' + e.latlng.lat);
+      let obj = {
+        lng: e.latlng.lng,
+        lat: e.latlng.lat
+      }
+      this.list.push(obj)
+      let arr= []
+      this.list.forEach(item=>{
+        arr.push(new BMapGL.Point(item.lng, item.lat))
+      })
+      var polyline = new BMapGL.Polyline(arr, { strokeColor: "blue", strokeWeight: 2, strokeOpacity: 0.5 });
+      map.addOverlay(polyline);
+    });
 
-    },
+  },
+  data() {
+    return {
+      list: []
+    }
+  },
+  methods: {
+
+  },
 }
 </script>
 
 <style lang="scss" scoped>
-#container{
-    width: 700px;
-    height: 700px;
+#container {
+  width: 700px;
+  height: 700px;
 }
 </style>
