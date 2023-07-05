cdc3afb427ab32b8a8e5594cc5b1d61eb75e14d1
Cao Zhijie <1051029537@qq.com>
2023-07-05 20:51:26 +0800

    update

--- dist/assets/ellipsis.html-2405dcf3.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,r as t,o as l,c,a as o}from"./app-4416975f.js";const n={};function _(r,i){const e=t("c-ellipsis");return l(),c("div",null,[o(e,{length:10,text:"呵呵呵呵呵呵呵呵呵呵哈哈哈哈哈哈哈哈还好"})])}const p=s(n,[["render",_],["__file","ellipsis.html.vue"]]);export{p as default};
