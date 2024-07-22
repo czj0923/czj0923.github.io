6d3fb40227cfb0ad257dd7100a0802b4a179ccd1
Cao Zhijie <1051029537@qq.com>
2024-07-22 10:19:16 +0800

    build

---
+++ dist/assets/ellipsis.html-e84d7cfa.js
@@ -0,0 +1,1 @@
+import{_ as s,r as t,o as l,c,a as o}from"./app-81297fa3.js";const n={};function _(r,i){const e=t("c-ellipsis");return l(),c("div",null,[o(e,{length:10,text:"呵呵呵呵呵呵呵呵呵呵哈哈哈哈哈哈哈哈还好"})])}const p=s(n,[["render",_],["__file","ellipsis.html.vue"]]);export{p as default};
