1f801a888f685636e5370743f01749cd58b8af46
czj0923 <45352719+czj0923@users.noreply.github.com>
2025-07-28 09:10:05 +0000

    Deploying to gh-pages from @ czj0923/czj0923.github.io@20477d655bda83ff4ed0ed4390bbc97f8ad1b741 🚀

--- assets/PBChart-4kUsnIlf.js
+++
@@ -1,9 +0,0 @@
-import{i as o,p as i}from"./run-record-DA6eC6ts.js";import{_ as p,c,o as d}from"./app-BhyDz4z2.js";let n=null;const m={name:"PBChart",data(){return{pbRecord:i}},mounted(){this.initChart()},methods:{initChart(){const e=this.pbRecord.map(t=>`${t.year}-${t.month}-${t.day}`),s=this.pbRecord.map(t=>{const a=t.time.split(":");return a[0]*3600+a[1]*60+a[2]*1});n=o(this.$refs.chart),n.setOption({xAxis:{type:"category",data:e},yAxis:{type:"value",axisLabel:{formatter:t=>this.sceond2time(t)},min:4800,max:7200},tooltip:{trigger:"axis",axisPointer:{type:"shadow"},formatter:t=>{const a=t[0];return`<div>
-              <span>用时:</span><span>${this.sceond2time(a.value)}</span>
-              </div>
-              <div>
-              <span>日期:</span><span>${a.axisValue}</span>
-              </div>
-            <div>
-                <span>地点:</span><span>${this.pbRecord[a.dataIndex].addr}</span>
-              </div>`}},series:[{name:"pb",data:s,type:"line",smooth:!0}]})},sceond2time(e){const s=(e-e%3600)/3600,t=e%60,a=(e-s*3600-t)/60;return`${s?s+":":""}${a}:${t}`}}},h={ref:"chart",id:"my-chart"};function l(e,s,t,a,r,u){return d(),c("div",h,null,512)}const _=p(m,[["render",l],["__scopeId","data-v-03bcb557"]]);export{_ as default};
---
+++ assets/Template-DShJZLRm.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o}from"./app-Dlxm6KHK.js";const r={data(){return{}},methods:{}};function a(c,n,s,p,_,d){return o(),t("div",null,"你好")}const f=e(r,[["render",a]]);export{f as default};
