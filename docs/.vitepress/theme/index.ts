013554e9b63a654afca2b0341c4e71f744cfde85
Cao Zhijie <1051029537@qq.com>
2025-08-12 23:52:46 +0800

    update

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,4 +1,5 @@
-import BlogTheme from '@sugarat/theme'
+import BlogTheme from '@sugarat/theme';
+const modules = import.meta.glob('./components/**/*.vue');
 
 // 自定义样式重载
 //import './style.scss'
@@ -6,4 +7,18 @@ import BlogTheme from '@sugarat/theme'
 // 自定义主题色
 //import './user-theme.css'
 
-export default BlogTheme
+export default {
+  ...BlogTheme,
+  enhanceApp({ app }) {
+    // 批量注册components文件夹下的组件
+    for (const path in modules) {
+      modules[path]().then((mod) => {
+        const reg = /^\.\/components\/([a-zA-Z-]+)\.vue$/;
+        const matches = path.match(reg);
+        if (matches) {
+          app.component(matches[1], mod.default);
+        }
+      });
+    }
+  }
+};
